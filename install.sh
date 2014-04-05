c776c6987772e099dac5d0f73c41673d6cc085d8
Luis <ssuser@llopez-mbp.local>
2014-04-04 20:57:31 -0400

    installer script + aliases

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+[[ ! -f ~/.inputrc ]] && cp .inputrc ~/
+[[ ! -f ~/.tmux.conf ]] && cp .tmux.conf ~/
+[[ ! -f ~/.vimrc ]] && cp .vimrc ~/
+[[ ! -f ~/.bashrc ]] && cp .bashrc ~/
+
+ 
